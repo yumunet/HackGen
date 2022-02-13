0d82670d8ed937c39609884f2b76029031ec4116
yumu <93029942+yumunet@users.noreply.github.com>
2022-02-13 20:04:05 +0900

    Revert centering of full-width round brackets

--- hackgen_generator.sh
+++ hackgen_generator.sh
@@ -1293,8 +1293,6 @@ while (i < SizeOf(input_list))
   # Edit zenkaku brackets
   Print("Edit zenkaku brackets")
   bracket_move = $((${hackgen_half_width} / 2 + ${hackgen_half_width} / 30))
-  Select(0uff08); Move(-bracket_move, 0); SetWidth(${hackgen_full_width}) # (
-  Select(0uff09); Move( bracket_move, 0); SetWidth(${hackgen_full_width}) # )
   Select(0uff3b); Move(-bracket_move, 0); SetWidth(${hackgen_full_width}) # [
   Select(0uff3d); Move( bracket_move, 0); SetWidth(${hackgen_full_width}) # ]
   Select(0uff5b); Move(-bracket_move, 0); SetWidth(${hackgen_full_width}) # {
@@ -1463,8 +1461,6 @@ while (i < SizeOf(input_list))
   # Edit zenkaku brackets
   Print("Edit zenkaku brackets")
   bracket_move = $((${hackgen35_half_width} / 2 + ${hackgen35_half_width} / 30))
-  Select(0uff08); Move(-bracket_move, 0); SetWidth(${hackgen35_full_width}) # (
-  Select(0uff09); Move( bracket_move, 0); SetWidth(${hackgen35_full_width}) # )
   Select(0uff3b); Move(-bracket_move, 0); SetWidth(${hackgen35_full_width}) # [
   Select(0uff3d); Move( bracket_move, 0); SetWidth(${hackgen35_full_width}) # ]
   Select(0uff5b); Move(-bracket_move, 0); SetWidth(${hackgen35_full_width}) # {
